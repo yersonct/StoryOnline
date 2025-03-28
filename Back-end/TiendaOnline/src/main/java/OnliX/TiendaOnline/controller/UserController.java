de0adb6e05f37e8568b147d390198eb5ed6c09c8
justin daniela <dannybahamon613@gmail.com>
2025-03-28 17:44:48 -0500

    subir

--- Back-end/TiendaOnline/src/main/java/OnliX/TiendaOnline/controller/UserController.java
+++ Back-end/TiendaOnline/src/main/java/OnliX/TiendaOnline/controller/UserController.java
@@ -1,5 +1,47 @@
 package OnliX.TiendaOnline.controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import OnliX.TiendaOnline.DTO.requestRegisterUser;
+import OnliX.TiendaOnline.service.UserService;
+
+@RestController
+@RequestMapping("api/V1/user")
+public class UserController {
+    /*
+     * GET = Obtener datos o constutar
+     * POST = Crear un registro
+     * PUT = Actualización total
+     * PATCH = Actualización parcial
+     * DELETE = Eliminar
+     */
+
+     @Autowired
+
+     private UserService UserService;
+
+     @GetMapping("/")
+     public ResponseEntity<Object> findAllUser(){
+        var ListUser = UserService.findAllUSer();
+        return new ResponseEntity<Object>(ListUser,HttpStatus.OK);
+     }
+
+     @GetMapping("/{Id}")
+     public ResponseEntity<Object> findByEntity(@PathVariable int id){
+        var user = UserService.findByIdUser(id);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+     }
+     @PostMapping("/")
+     public String postMethodName(@RequestBody requestRegisterUser user){
+        UserService.save(user);
+        return "Register Ok";
+     }
 }
--- Back-end/TiendaOnline/src/main/java/OnliX/TiendaOnline/service/UserService.java
+++ Back-end/TiendaOnline/src/main/java/OnliX/TiendaOnline/service/UserService.java
@@ -1,11 +1,57 @@
 package OnliX.TiendaOnline.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import OnliX.TiendaOnline.DTO.requestRegisterUser;
+import OnliX.TiendaOnline.Interfaces.IUser;
+import OnliX.TiendaOnline.model.user;
 
 @Service
 public class UserService {
 
     /*
-     * @Auot
+     * @Autowired = Incluye la conexion de la interface
      */
+    @Autowired
+    private  IUser UserData;
+    public List<user> findAllUSer(){
+        return UserData.findAll(); // obtener todos los usuario 
+    }
+
+    public Optional<user> findByIdUser(int id){
+        return UserData.findById(id);
+    }
+
+    public void save(requestRegisterUser user){
+        UserData.save(convertRegisterToUser(user));
+    }
+
+    public user convertRegisterToUser(requestRegisterUser user){
+      return new user(0, 
+        user.getName(),
+        user.getEmail(),
+        null,
+        null,
+        null,
+        user.getRole()
+        );
+    }
+    public void update(int id,user userUpdate){
+        var user = findByIdUser(id);
+        if(user.isPresent()){
+            user.get().setName(userUpdate.getName());
+            user.get().setEmail(userUpdate.getEmail());
+            user.get().setRole(userUpdate.getRole());
+        }
+    }
+    public void Delete(int id){
+        var user =  findByIdUser(id);
+        if(user.isPresent()){
+            UserData.delete(user.get());
+        }
+    }
 }
