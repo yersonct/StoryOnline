733e2db3f21e608c4293a8be83545c8d309ded83
yerson stiven cuellar rubiano <yersonstivencuellarrubiano@gmail.com>
2025-03-28 20:40:12 -0500

    subir

--- Back-end/TiendaOnline/src/main/java/OnliX/TiendaOnline/service/supplierService.java
+++ Back-end/TiendaOnline/src/main/java/OnliX/TiendaOnline/service/supplierService.java
@@ -1,5 +1,35 @@
 package OnliX.TiendaOnline.service;
 
-public class SupplierService {
+import java.util.List;
+import java.util.Optional;
 
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import Onlix.tiendaOnline.DTO.requestRegisterSupplier
+import OnliX.TiendaOnline.Interfaces.ISupplier;
+import OnliX.TiendaOnline.model.supplier;
+
+@Service
+public class supplierService {
+    /*
+     * @Autowired = Incluye la conexion de la interface
+     */
+
+    @Autowired
+    public ISupplier supplierData;
+    public List<supplier> findAllSupplier(){
+        return supplierData.findAll();
+    }
+
+    public Optional<supplier> findByIdSupplier(int id){
+        return supplierData.findById(id);
+    }
+
+    public void save(requestRegisterSupplier supplier){
+        supplierData.save(convertRegisterToSupplier(supplier));
+    }
+
+    public supplier convertRegisterToSupplier(requestRegisterSupplier supplier){
+        return new supplier()
+    }
+}   
