f8d1ad87e3777eb253914d01655b2379f128f9be
yerson stiven cuellar rubiano <yersonstivencuellarrubiano@gmail.com>
2025-03-28 23:01:39 -0500

    subir

--- Back-end/TiendaOnline/src/main/java/OnliX/TiendaOnline/service/ShipmentService.java
+++ Back-end/TiendaOnline/src/main/java/OnliX/TiendaOnline/service/ShipmentService.java
@@ -1,10 +1,12 @@
 package OnliX.TiendaOnline.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import OnliX.TiendaOnline.DTO.requestRegisterShipment;
 import OnliX.TiendaOnline.Interfaces.IShipment;
 import OnliX.TiendaOnline.model.shipment;
 
@@ -19,10 +21,13 @@ public class ShipmentService {
         return shipmentData.findAll();
     }
     public Optional<shipment> findByIdShipment(int id){
-        return shipmentData,findAll();
+        return shipmentData.findById(id);
     }
 
     public void save(requestRegisterShipment shipment){
         shipmentData.save(convertRegisterToShipment(shipment));
+    }
+    public shipment convertRegisterToShipment(requestRegisterShipment shipment){
+        return new shipment(0, null, true,shipment.getCompanyShipping(), null, null);
     }
 }
